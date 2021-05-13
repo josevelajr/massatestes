2c09aa241f282fb3f252ac2e7aeb506f4b815006
jvela <Asd@147258>
2021-05-13 18:08:28 -0300

    Inclusao do chromedriver

---
+++ src/test/java/AbrirSelenium.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AbrirSelenium {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/jvela/IdeaProjects/cursoMassa/cursoMassa/src/drivers/chromedriver_linux64/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+
+    }
+
+
+}
