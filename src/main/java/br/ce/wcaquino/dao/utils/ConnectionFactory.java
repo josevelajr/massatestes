0ccdd56723596d95fb1feb6937340654a3044f74
jvela <Asd@147258>
2021-05-13 19:46:37 -0300

    Inclusao do Postgres

--- src/main/java/br/ce/wcaquino/dao/utils/ConnectionFactory.java
+++ src/main/java/br/ce/wcaquino/dao/utils/ConnectionFactory.java
@@ -12,7 +12,7 @@ public class ConnectionFactory {
 	private static String porta = "5432"; 
 	private static String schema = "dbunit"; 
 	private static String user = "postgres"; 
-	private static String senha = "passwd"; 
+	private static String senha = "Postgres@147258";
 	
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		if(conn == null) {
--- src/test/java/AbrirSelenium.java
+++ src/test/java/AbrirSelenium.java
@@ -1,10 +1,24 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class AbrirSelenium {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/home/jvela/IdeaProjects/cursoMassa/cursoMassa/src/drivers/chromedriver_linux64/chromedriver");
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://seubarriga.wcaquino.me/");
+        driver.findElement(By.id("email")).sendKeys("jose.vela.jr@outlook.com");
+        driver.findElement(By.id("senha")).sendKeys("123456");
+        driver.findElement(By.tagName("button")).click();
+
+        driver.quit();
+
+
+
+
 
     }
 
